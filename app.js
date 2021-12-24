ed0bc4288dd60dc4d1a1e3d6e05214c4ec7a5570
Natarajan <mahesh.a.natarajan@capgemini.com>
2021-12-24 08:05:55 -0600

    base 2

--- app.js
+++ app.js
@@ -2,7 +2,34 @@ const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb://localhost:27017";
+const DATABASE_NAME = "local";
+
+
 var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-app.listen(5000, () => {});
+var database, collection;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("books");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.post("/books", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+//curl -X POST -H 'content-type:application/json' -d '{"id":"4","title":"Book 4"}' http://localhost:5000/books
+//git bash
